9e33d0d0a6f3cf133f64300339747b52843f68a8
Do Tuan Anh <dotuananh.dta@gmail.com>
2020-06-22 13:50:49 +0700

    Create a factory

---
+++ lib/Shape.dart
@@ -0,0 +1,43 @@
+import 'dart:math';
+
+// Create a top-level Factory
+Shape shapeFactory(String type) {
+  if (type == 'circle') return Circle(2);
+  if (type == 'square') return Square(2);
+  throw 'Can\'t crate $type.';
+}
+
+abstract class Shape {
+  // Create a Factory Constructor
+  factory Shape(String type){
+    if (type == 'circle') return Circle(2);
+    if (type == 'square') return Square(2);
+    throw 'Can\'t create $type';
+  }
+  num get area;
+}
+
+class Circle implements Shape {
+  final num radius;
+  Circle(this.radius);
+  num get area => pi * pow(radius, 2);
+}
+
+class Square implements Shape {
+  final num side;
+  Square(this.side);
+  num get area => pow(side, 2);
+}
+
+main() {
+  // Use top-level Factory
+  final circle = shapeFactory('circle');
+  final square = shapeFactory('square');
+
+  // Use a Factory Constructor
+  final circle = Shape('circle');
+  final square = Shape('square');
+
+  print(circle.area);
+  print(square.area);
+}
