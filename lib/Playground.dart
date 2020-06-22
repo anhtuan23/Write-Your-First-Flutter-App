135885a132bbf72cec8e2c7b2be8fcd742c789a2
Do Tuan Anh <dotuananh.dta@gmail.com>
2020-06-22 10:26:22 +0700

    Define a Bicycle constructor

--- lib/Playground.dart
+++ lib/Playground.dart
@@ -3,6 +3,18 @@ class Bicycle {
   int cadence;
   int speed;
   int gear;
+
+  // This constructor has no body, which is valid in Dart
+  // Using this in a constructor's parameter list is a handy shortcut
+  // for assigning values to instance variables
+  Bicycle(this.cadence, this.speed, this.gear);
+
+  // The code above is equivalent to the following:
+//  Bicycle(int cadence, int speed, int gear) {
+//    this.cadence = cadence;
+//    this.speed = speed;
+//    this.gear = gear;
+//  }
 }
 
 // Dart's main method is named main() or,
