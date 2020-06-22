526bdc3045a1d9ab7167a874a6778ba6118028be
Do Tuan Anh <dotuananh.dta@gmail.com>
2020-06-22 10:22:09 +0700

    Define a Bicycle class

---
+++ lib/Playground.dart
@@ -0,0 +1,10 @@
+//All identifiers are public by default
+class Bicycle {
+  int cadence;
+  int speed;
+  int gear;
+}
+
+// Dart's main method is named main() or,
+// To access to command-line arguments, main(List<String> args)
+void main() {}
